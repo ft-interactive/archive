fbc5679dd454cd7da6aa15969921c69afc256af8
Martin Stabe <martinstabe@gmail.com>
2017-06-21 11:47:09 +0100

    Fix onward journey

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,5 @@
 export default () => ({
   relatedContent: [
-    { rows: 1, list: 'thing/38afe3a0-d51b-31fc-a886-1519fc60ef09' },
-	{ rows: 1, list: 'thing/fc9322ae-ef51-3e0b-9b40-7cfc016f3d4d' },  
+    { rows: 2, list: 'thing/MTY=-U2VjdGlvbnM=' },
   ],
 });
