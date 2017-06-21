4e01ddb29027269274fe1d21feb21265a663f1fe
Martin Stabe <martinstabe@gmail.com>
2017-06-21 20:09:14 +0100

    deduce onward journey

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,6 @@
 export default () => ({
   relatedContent: [
-    { rows: 1, list: 'thing/TnN0ZWluX0dMX1NB-R0w=' },
-    { rows: 1, list: 'thing/OTVjNjRjN2MtZTUxZi00NDM3LWIzOWUtNDEzZGYxYTA3ZTU5-UE4=' },
+    { rows: 2, list: 'thing/TnN0ZWluX0dMX1NB-R0w=' },
+    // { rows: 1, list: 'thing/OTVjNjRjN2MtZTUxZi00NDM3LWIzOWUtNDEzZGYxYTA3ZTU5-UE4=' },
   ],
 });
