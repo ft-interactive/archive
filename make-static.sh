e75a9c233e780f229fc46b213895b93fe09efb0c
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-04-24 14:31:00 +0100

    Rewriting internal links to point at dirs instead of .html

--- make-static.sh
+++ make-static.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v wget)" ] || ! [ -x "$(command -v rename)" ]; then
-	brew install rename wget
+	brew install rename wget gnu-sed
 fi
 
 
@@ -12,3 +12,11 @@ cd dist/us-elections
 find . -name '*.html' -exec sh -c 'mkdir `basename "$0" .html` && mv "$0" `basename "$0" .html`/index.html' '{}' \;
 
 find . -name index.html -exec gsed -i -e 's/src="forecast-map.svg"/src="..\/forecast-map.svg"/' {} \;
+find . -name index.html -exec gsed -i -e "s/'main\.entry\.js','g-ui\/index\.js'/'\.\.\/main\.entry.js','\.\.\/g-ui\/index.js'/" {} \;
+find . -name index.html -exec gsed -i -e 's/href="g-ui\/critical\.css"/href="\.\.\/g-ui\/critical\.css"/' {} \;
+find . -name index.html -exec gsed -i -e 's/src="\.\.\/static\/g-ui\/o-errors\.20161025\.js"/src="\.\.\/g-ui\/o-errors\.20161025\.js"/' {} \;
+find . -name index.html -exec gsed -i -e 's/href="main\.css"/href="\.\.\/main\.css"/' {} \;
+find . -name index.html -exec gsed -i -e 's/href="g-ui\/main\.css"/href="\.\.\/g-ui\/main\.css"/' {} \;
+find . -name index.html -exec gsed -i -e 's/src="g-ui\/top\.js"/src="\.\.\/g-ui\/top\.js"/' {} \;
+
+find . -name "index.html" -exec gsed -i -e 's/href="\([^\.]*\)\.html"/href="..\/\1\/"/' {} \;
